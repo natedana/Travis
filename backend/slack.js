d414e03ba6b9d4ebe5898841db21a786fee83f14
jeff <tangj1122@gmail.com>
2017-10-24 17:05:57 -0700

    scheduler

--- backend/slack.js
+++ backend/slack.js
@@ -2,22 +2,20 @@ var RtmClient = require('@slack/client').RtmClient;
 var ChatClient = require('@slack/client').ChatClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-var axios = require('axios');
+var schedule = require('node-schedule');
+
 var bot_token = process.env.SLACK_BOT_TOKEN || '';
 
 var rtm = new RtmClient(bot_token);
 
-let channel = 'D7Q6BLS30';
+// let channel = 'D7Q6BLS30'; // Nate-Bot
+let channel = 'D7Q8QTULE'; // Jeff-Bot
 
-rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-  // console.log(message);
-  // if (message.user === 'U7NPK6TEV') {
-  //   return
-  // }
-  console.log('User %s posted a message in %s channel', rtm.dataStore.getUserById(message.user).name, rtm.dataStore.getChannelGroupOrDMById(message.channel).name);
-  rtm.sendMessage('test send', channel)
-  // rtm.postMessage('test post', channel)
-  rtm.send(testMsg, channel)
+// why is the last message always logged on start?
+rtm.on(RTM_EVENTS.MESSAGE, (message) => {
+  console.log(`user: ${message.user}, text: ${message.text}, channel: ${message.channel}`);
+  rtm.sendMessage('hello world', channel);
 });
+
 
 rtm.start();
