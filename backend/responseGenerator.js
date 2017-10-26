9a57af1e96fe6d722e55ab548aa76f9f8fb8a147
jeff <tangj1122@gmail.com>
2017-10-26 11:05:02 -0700

    refactor, play around with default messages

--- backend/responseGenerator.js
+++ backend/responseGenerator.js
@@ -2,6 +2,7 @@
 //   data:
 // };
 
+// what is DialogFlow's default data anyways?
 const newRes = (displayText, data = {}, contextOut = [], source = '') => {
   return {
     speech: displayText,
--- backend/routes.js
+++ backend/routes.js
@@ -14,7 +14,6 @@ router.get('/', (req, res) => {
 });
 
 router.post('/fulfillment', (req, res, next) => {
-  console.log('\n/fulfillment', req.body);
   const result = req.body.result;
   let displayText;
 
@@ -22,12 +21,10 @@ router.post('/fulfillment', (req, res, next) => {
     case 'save-term.confirm':
       Term.create({ termEN: result.parameters.term })
         .then(resp => {
-          console.log('\nterm created', resp);
           displayText = `${result.parameters.term} saved to your profile 🔥`;
           res.json({ speech: displayText, displayText });
         })
         .catch(err => {
-          console.log('\nError: term not created', err);
           displayText = `Uh oh, something went wrong.`;
           res.json({ speech: displayText, displayText });
         });
@@ -39,59 +36,32 @@ router.post('/fulfillment', (req, res, next) => {
       case 'request-list':
        Term.find().limit(10).exec((err, results) => {
          if (!results) {
-           res.json(newRes('No List Pal'))
+           displayText = 'No list pal';
+           res.json({ speech: displayText, displayText });
          } else {
-           let text = 'Your terms:'
+           displayText = 'Your terms:';
            results.forEach(term => {
-             text += `\n-${term.termEN}`
-           })
-           console.log(text);
-          //  res.json({ speech: text, displayText: text }); // original
-
-          // default messages
-           res.send({"messages": [
-             {
-               "speech": "Text response",
-               "type": 0
-             }
-           ]});
+             displayText += `\n - ${term.termEN}`;
+           });
+          //  using DialogFlow "Fulfillment Response" (https://dialogflow.com/docs/fulfillment#response)
+           res.json({ speech: displayText, displayText });
+          // using DialogFlow "default messages" (https://dialogflow.com/docs/reference/agent/message-objects#one-click_integration_message_objects)
+          //  res.send({"messages": [
+          //    {
+          //      "speech": "Text response",
+          //      "type": 0
+          //    }
+          //  ]});
          }
        }).catch(err => {
-         res.json(newRes("Error:" + err))
+         displayText = `Error: ${err}`;
+         res.json({ speech: displayText, displayText });
        });
        break;
     default:
       console.log('default passed');
       res.send('default passed');
       break;
-  }
-})
-
-router.post('/interactive', (req, res, next) => {
-  console.log("PAYLOAD\n\n\n",req.body.payload);
-  const parsed = JSON.parse(req.body.payload); //tests vs. real might have diff data structure
-  console.log('parsed payload', parsed);
-  switch (parsed.callback_id) {
-    case('CONFIRM_NEW_TERM'): // we need to check if name of button is save or reject TODO
-      console.log('confirm new term selected:');
-      delaySeconds(5000,() => {
-        console.log('5 second delay');
-      })
-      if (parsed.actions[0].value[0] === '1') {
-        Term.create({termEN: parsed.actions[0].value.split('_')[1].slice(3), termCN: parsed.actions[0].value.split('_')[2].slice(3)})
-          .then(resp => {
-            console.log("word response",resp);
-            res.json({success: true, text: `Your term ${resp.termEN} -> ${resp.termCN} saved!🔥`})
-          })
-          .catch(err => res.json({success: false, text: 'Your term did not save 😔'}));
-      } else {
-        res.json({success: true, text: 'That\'s ok maybe next time.'})
-      }
-      break;
-    default:
-      console.log('default passed \n \n Parsed payload');
-      console.log(parsed);
-      res.json({success: false, text: 'Hmm, something went wrong with your interactive'});
   }
 })
 
