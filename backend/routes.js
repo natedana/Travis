9fff860317a2d566ef07c989769ea1b9829b44ff
jeff <tangj1122@gmail.com>
2017-10-26 16:45:10 -0700

    more

--- backend/routes.js
+++ backend/routes.js
@@ -132,8 +132,8 @@ router.post('/fulfillment', (req, res, next) => {
       console.log(result)
       const answer = result.contexts.parameters.answer;
       const termEN = result.contexts.parameters.term;
-      // console.log('\nq1res', q1res, answer, termEN);
-      // let q1res = answer === termEN ? "✔️" : `Ⅹ - ${termEN}`;
+      let q1res = answer === termEN ? "✔️" : `Ⅹ - ${termEN}`;
+      console.log('q1res', q1res, answer, termEN);
       // res.json({speech: q1res, displayText: q1res});
       // Term.count().exec((err, count) => {
       //   var random = Math.floor(Math.random() * count);
