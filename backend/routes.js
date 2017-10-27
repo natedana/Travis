766c62105d44043709eb5fb1434f6b41c07a6bf8
jeff <tangj1122@gmail.com>
2017-10-27 16:32:49 -0700

    finish demo

--- backend/routes.js
+++ backend/routes.js
@@ -2,14 +2,12 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const _ = require('underscore');
-const delaySeconds = require('./delay').delaySeconds;
 const fs = require('fs');
 const path = require('path');
+// const delaySeconds = require('./delay').delaySeconds;
 
 const Term = require('./models').Term;
 const Exam = require('./models').Exam;
-
-/* const newRes = require('./responseGenerator');// TODO use? */
 
 router.get('/', (req, res) => {
   res.send('hello world');
@@ -17,8 +15,8 @@ router.get('/', (req, res) => {
 
 router.post('/fulfillment', (req, res) => {
   const result = req.body.result;
+  console.log('\n\nDialogFlow result:\n', result);
   let displayText;
-  let q1, q2, q3;// TODO can we turn quiz into a repeatable function?
 
   switch (result.action) {
     case 'save-term.confirm':
@@ -119,12 +117,17 @@ router.post('/fulfillment', (req, res) => {
       Term.find().exec((err, Terms) => {
         newExam.questions =
           _.shuffle(Terms)
-          .slice(0, newExam.examLength)
-          .map(termObj => ({ prompt:termObj.termEN, answer:termObj.termCN }));
+           .slice(0, newExam.examLength)
+           .map(termObj => ({
+             prompt:termObj.termEN,
+             answer:termObj.termCN
+           }));
         newExam.save(err => {
           if (!err) {
-            displayText = `Q1: Translate ${newExam.questions[0].prompt} to Chinese.`
-            res.json({speech: displayText, displayText,
+            displayText = `Q1) Translate ${newExam.questions[0].prompt} to Chinese (1/${newExam.examLength})`
+            res.json({
+              speech: displayText,
+              displayText,
               contextOut: [
                 {
                   name: 'exam-followup',
@@ -142,17 +145,19 @@ router.post('/fulfillment', (req, res) => {
     }
     case 'exam-followup':
       const examData = {...result.contexts[0].parameters.examData};
-      console.log('\ncurrent', result.parameters.answer, examData.currentIndex, examData.questions[examData.currentIndex].answer);
+      // console.log('\ncurrent', result.parameters.answer, examData.currentIndex, examData.questions[examData.currentIndex].answer);
       if (examData.questions[examData.currentIndex].answer.toLowerCase() === result.parameters.answer.toLowerCase()) {
         examData.score += 1;
       }
       if (examData.currentIndex + 1 === examData.examLength) {
         displayText = `Finished! You got ${examData.score} out of ${examData.examLength}`;
-        res.json({speech: displayText, displayText});
+        res.json({ speech: displayText, displayText });
       } else {
         examData.currentIndex += 1;
-				displayText = `Q${examData.currentIndex + 1}: Translate ${examData.questions[examData.currentIndex].prompt} to Chinese.`;
-				res.json({speech: displayText, displayText,
+				displayText = `Q${examData.currentIndex + 1}: Translate ${examData.questions[examData.currentIndex].prompt} to Chinese (${examData.currentIndex + 1}/${examData.examLength})`;
+				res.json({
+          speech: displayText,
+          displayText,
 					contextOut: [
 						{
 							name: 'exam-followup',
@@ -164,23 +169,32 @@ router.post('/fulfillment', (req, res) => {
 					]
         });
       }
-    break;
+      break;
     default:
       res.send('default passed');
       break;
   }
 });
 
-router.post('/delete', (req, res) => {
-  Term.remove({termEN: req.body.text}).exec((err, b) => {
-    res.json({success: true, text: `Successfully deleted the term ${req.body.text}.`})
-  }).catch(err => {
-    res.json({
-      success: false,
-      text: `Something went wrong:` + err
-    })
-  })
-})
+router.get('/privacy_policy', (req, res) => {
+  fs.readFile(path.join('./public/privacy_policy.html'), 'utf8', (err, data) => {
+    if (err) console.log('Error reading file', err);
+    else {
+      res.send(data);
+    }
+  });
+});
+
+// router.post('/delete', (req, res) => {
+//   Term.remove({termEN: req.body.text}).exec((err, b) => {
+//     res.json({success: true, text: `Successfully deleted the term ${req.body.text}.`})
+//   }).catch(err => {
+//     res.json({
+//       success: false,
+//       text: `Something went wrong:` + err
+//     })
+//   })
+// })
 
 /* router.post('/list', (req, res) => { */
 /*   Term.find({}, (err, results) => { */
@@ -200,7 +214,6 @@ router.post('/delete', (req, res) => {
 /*     }) */
 /*   }) */
 /* }); */
-
 
 // router.get('/facebook_redirect', (req, res) => {
 //   const FB_APP_ID = '362280927530951';
@@ -231,14 +244,5 @@ router.post('/delete', (req, res) => {
 //     res.send(data);
 //   });
 // })
-
-router.get('/privacy_policy', (req, res) => {
-  fs.readFile(path.join('./public/privacy_policy.html'), 'utf8', (err, data) => {
-    if (err) console.log('Error reading file', err);
-    else {
-      res.send(data);
-    }
-  });
-});
 
 module.exports = router;
