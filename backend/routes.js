65051f8c4bf7f52c08d58e161382ec9d921946f1
jeff <tangj1122@gmail.com>
2017-10-26 17:00:20 -0700

    more

--- backend/routes.js
+++ backend/routes.js
@@ -138,8 +138,7 @@ router.post('/fulfillment', (req, res, next) => {
       Term.count().exec((err, count) => {
         var random = Math.floor(Math.random() * count);
         Term.findOne().skip(random).exec(function(err, term) {
-          displayText = [q1res,displayText].join(' ')
-          displayText = `What is ${result.termEN} in Chinese?`;
+          displayText = [q1res,`What is ${result.termEN} in Chinese?`;].join(' ')
           res.json({speech: displayText, displayText,
             contextOut: [
             {
