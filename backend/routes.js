72e43d00c39cda6756b8227e31bcf0cab2a24b66
jeff <tangj1122@gmail.com>
2017-10-26 17:04:51 -0700

    2x res json

--- backend/routes.js
+++ backend/routes.js
@@ -134,7 +134,6 @@ router.post('/fulfillment', (req, res, next) => {
       const termEN = result.contexts[0].parameters.term;
       let q1res = answer === termEN ? "✔️" : `Ⅹ - ${termEN}`;
       console.log('q1res', q1res, answer, termEN);
-      res.json({speech: q1res, displayText: q1res});
       Term.count().exec((err, count) => {
         var random = Math.floor(Math.random() * count);
         Term.findOne().skip(random).exec(function(err, term) {
